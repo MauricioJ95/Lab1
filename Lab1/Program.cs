3cd57630edfaf2221a67e68fca56657ec8932bd1
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-09-19 20:28:11 -0400

    Lab1 complete

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,6 +10,44 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Put in a 3 digit integer");
+            string int1 =
+                Console.ReadLine();
+            while (!IsThreeDigitiInt(int1))
+            {
+                Console.WriteLine("Please input a 3 digit integer");
+                int1 =
+                    Console.ReadLine();
+            }
+
+            Console.WriteLine("Put in second 3 digit integer");
+            string int2 =
+                Console.ReadLine();
+            while (!IsThreeDigitiInt(int2))
+            {
+                Console.WriteLine("Please input a 3 digit integer");
+                int2 =
+                    Console.ReadLine();
+            }
+
+            string output;
+            if (int1[0] + int2[0] == int1[1] + int2[1] && int1[1] + int2[1] == int1[2] + int2[2])
+            {
+                output = "True";
+            }
+            else
+            {
+                output = "False";
+            }
+            Console.WriteLine(output);
+            Console.ReadLine();
         }
+        private static bool IsThreeDigitiInt(string input)
+        {
+            int result;
+            return int.TryParse(input, out result) && input.Length == 3;
+
+        }
+
     }
 }
